ade4811cd734c88f82f0b0c6460c2b06aafbf2e7
Menaka J <menakaraman4@gmail.com>
2026-01-13 20:23:48 +0530

    cm4

--- src/main/java/code/logic/tamil/ems_backend/service/EmployeeService.java
+++ src/main/java/code/logic/tamil/ems_backend/service/EmployeeService.java
@@ -5,8 +5,10 @@ import code.logic.tamil.ems_backend.dto.EmployeeDto;
 import java.util.List;
 
 public interface EmployeeService {
+    //for creation
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
+    //for retrivel
     EmployeeDto getEmployeeByID(Long employeeId);
 
     List<EmployeeDto> getAllEmployees();
